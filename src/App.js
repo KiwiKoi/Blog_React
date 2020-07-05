8938f75beb510c43da0eeaf8d0d63e1652339b67
Daniel <danielo.visage@gmail.com>
2020-07-05 17:40:18 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Articles from "./Articles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    articles: [
+      {
+        id: 1,
+        title: "First Article",
+        contents: "This is the first article",
+        author: "Daniel",
+      },
+      {
+        id: 2,
+        title: "Second Article",
+        contents: "This is the second article",
+        author: "Daniel",
+      },
+      {
+        id: 3,
+        title: "Third Article",
+        contents: "This is the third article",
+        author: "Daniel",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React App!</h1>
+        <p>Welcome</p>
+        <Articles articles={this.state.articles} />
+      </div>
+    );
+  }
 }
 
 export default App;
